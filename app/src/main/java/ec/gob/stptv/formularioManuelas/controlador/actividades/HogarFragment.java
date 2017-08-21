d51ce8f216919acd0751837c9f7eff3b11cdc906
xacrys <cristiantintinmedina@hotmail.com>
2017-08-21 12:38:00 -0500

    Se realiza las validaciones en HogarFragment

--- app/src/main/java/ec/gob/stptv/formularioManuelas/controlador/actividades/HogarFragment.java
+++ app/src/main/java/ec/gob/stptv/formularioManuelas/controlador/actividades/HogarFragment.java
@@ -1,15 +1,21 @@
 package ec.gob.stptv.formularioManuelas.controlador.actividades;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 import ec.gob.stptv.formularioManuelas.R;
 import ec.gob.stptv.formularioManuelas.controlador.preguntas.HogarPreguntas;
+import ec.gob.stptv.formularioManuelas.controlador.util.Values;
 
 /***
  * Autor: Christian Tintin
@@ -17,21 +23,24 @@ import ec.gob.stptv.formularioManuelas.controlador.preguntas.HogarPreguntas;
 
 public class HogarFragment extends Fragment {
 
-    Spinner tipoHogarSpinner;
-    Spinner documentoHogarSpinner;
-    Spinner fuenteAguaSpinner;
-    Spinner ubicacionAguaSpinner;
-    Spinner tratamientoAguaSpinner;
-    Spinner servicioSanitarioSpinner;
-    Spinner ubicacionSanitarioSpinner;
-    Spinner servicioDuchaSpinner;
-    Spinner eliminaBasuraSpinner;
-    Spinner tipoAlumbradoSpinner;
-    Spinner energeticoCocinaSpinner;
-    EditText codigoElectricoEditText;
-    EditText numCuartosEditText;
-    EditText numDormitoriosEditText;
-
+    private Spinner tipoHogarSpinner;
+    private Spinner documentoHogarSpinner;
+    private Spinner fuenteAguaSpinner;
+    private Spinner ubicacionAguaSpinner;
+    private Spinner tratamientoAguaSpinner;
+    private Spinner servicioSanitarioSpinner;
+    private Spinner ubicacionSanitarioSpinner;
+    private Spinner servicioDuchaSpinner;
+    private Spinner eliminaBasuraSpinner;
+    private Spinner tipoAlumbradoSpinner;
+    private Spinner energeticoCocinaSpinner;
+    private EditText codigoElectricoEditText;
+    private EditText numCuartosEditText;
+    private EditText numDormitoriosEditText;
+    private Button guardarPersonaButton;
+    private RadioGroup gasParaCalefonOpcion;
+    private RadioGroup terrenoAgropecuario;
+    private RadioGroup terrenoAgropecuarioSi;
 
 
     @Override
@@ -46,16 +55,15 @@ public class HogarFragment extends Fragment {
 
         this.obtenerVistas(item);
         this.cargarPreguntas();
+        this.realizarAcciones();
 
 
         return item;
     }
 
 
-
     /**
      * Método para obtener las controles de la vista
-     *
      */
     private void obtenerVistas(View item) {
 
@@ -64,15 +72,22 @@ public class HogarFragment extends Fragment {
         numCuartosEditText = item.findViewById(R.id.numCuartosEditText);
         numDormitoriosEditText = item.findViewById(R.id.numDormitoriosEditText);
         fuenteAguaSpinner = item.findViewById(R.id.fuenteAguaSpinner);
-        ubicacionAguaSpinner=item.findViewById(R.id.ubicacionAguaSpinner);
-        tratamientoAguaSpinner=item.findViewById(R.id.tratamientoAguaSpinner);
-        servicioSanitarioSpinner=item.findViewById(R.id.servicioSanitarioSpinner);
+        ubicacionAguaSpinner = item.findViewById(R.id.ubicacionAguaSpinner);
+        tratamientoAguaSpinner = item.findViewById(R.id.tratamientoAguaSpinner);
+        servicioSanitarioSpinner = item.findViewById(R.id.servicioSanitarioSpinner);
         ubicacionSanitarioSpinner = item.findViewById(R.id.ubicacionSanitarioSpinner);
-        servicioDuchaSpinner=item.findViewById(R.id.servicioDuchaSpinner);
+        servicioDuchaSpinner = item.findViewById(R.id.servicioDuchaSpinner);
         eliminaBasuraSpinner = item.findViewById(R.id.eliminaBasuraSpinner);
         tipoAlumbradoSpinner = item.findViewById(R.id.tipoAlumbradoSpinner);
         codigoElectricoEditText = item.findViewById(R.id.codigoElectricoEditText);
         energeticoCocinaSpinner = item.findViewById(R.id.energeticoCocinaSpinner);
+        guardarPersonaButton = item.findViewById(R.id.guardarPersonaButton);
+        gasParaCalefonOpcion = item.findViewById(R.id.gasParaCalefon);
+        terrenoAgropecuario = item.findViewById(R.id.terrenoAgropecuario);
+        terrenoAgropecuarioSi = item.findViewById(R.id.terrenoAgropecuarioSi);
+
+
+
     }
 
     /**
@@ -126,10 +141,164 @@ public class HogarFragment extends Fragment {
 
     /**
      * valida campos obligatorios, numeros de telefonos etc.
-     *
      */
     protected boolean validarCampos() {
+        boolean cancel = false;
+        View focusView = null;
+        if (((Values) tipoHogarSpinner.getSelectedItem()).getKey().equals("-1")) {
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.tenenciaHogar));
+            cancel = true;
+            return cancel;
+        }
+
+        if ((((Values) tipoHogarSpinner.getSelectedItem()).getKey().equals("1") ||
+                ((Values) tipoHogarSpinner.getSelectedItem()).getKey().equals("2")) &&
+                        ((Values) documentoHogarSpinner.getSelectedItem()).getKey().equals("-1")) {
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.documentoHogar));
+            cancel = true;
+            return cancel;
+        }
+
+        if (numCuartosEditText.getText().toString().trim().equals(""))
+        {
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.cuartos));
+            focusView = numCuartosEditText;
+            cancel = true;
+            return cancel;
+        }
+        if (numDormitoriosEditText.getText().toString().trim().equals(""))
+        {
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.dormitorios));
+            focusView = numDormitoriosEditText;
+            cancel = true;
+            return cancel;
+        }
+
+        if (((Values) fuenteAguaSpinner.getSelectedItem()).getKey().equals("-1")) {
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.fuenteAgua));
+            cancel = true;
+            return cancel;
+        }
+
+        if (((Values) ubicacionAguaSpinner.getSelectedItem()).getKey().equals("-1")) {
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.ubicacionAgua));
+            cancel = true;
+            return cancel;
+        }
+
+        if (((Values) tratamientoAguaSpinner.getSelectedItem()).getKey().equals("-1")) {
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.tratamientoAgua));
+            cancel = true;
+            return cancel;
+        }
+
+        if (((Values) servicioSanitarioSpinner.getSelectedItem()).getKey().equals("-1")) {
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.servicioSanitario));
+            cancel = true;
+            return cancel;
+        }
+
+        if (((Values) ubicacionSanitarioSpinner.getSelectedItem()).getKey().equals("-1") &&
+                !((Values) servicioSanitarioSpinner.getSelectedItem()).getKey().equals("6")
+                ) {
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.ubicacionSanitario));
+            cancel = true;
+            return cancel;
+        }
+        if (((Values) servicioDuchaSpinner.getSelectedItem()).getKey().equals("-1")) {
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.servicioDucha));
+            cancel = true;
+            return cancel;
+        }
+        if (((Values) eliminaBasuraSpinner.getSelectedItem()).getKey().equals("-1")) {
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.eliminaBasura));
+            cancel = true;
+            return cancel;
+        }
+        if (((Values) tipoAlumbradoSpinner.getSelectedItem()).getKey().equals("-1")) {
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.tipoAlumbrado));
+            cancel = true;
+            return cancel;
+        }
+        if (codigoElectricoEditText.getText().toString().trim().equals("") &&
+        ((Values) tipoAlumbradoSpinner.getSelectedItem()).getKey().equals("1"))
+        {
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.codigoElectrico));
+            focusView = codigoElectricoEditText;
+            cancel = true;
+            return cancel;
+        }
+        if (((Values) energeticoCocinaSpinner.getSelectedItem()).getKey().equals("-1")) {
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.energeticoCocina));
+            cancel = true;
+            return cancel;
+        }
+
+        if(gasParaCalefonOpcion.getCheckedRadioButtonId()== -1){
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.gasParaCalefon));
+            cancel = true;
+            return cancel;
+        }
+
+        if(terrenoAgropecuario.getCheckedRadioButtonId()== -1){
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.terrenoAgropecuario));
+            cancel = true;
+            return cancel;
+        }
+
+        if(terrenoAgropecuarioSi.getCheckedRadioButtonId()== -1 &&
+                terrenoAgropecuario.getCheckedRadioButtonId() == R.id.terrenoAgropecuarioOpcion1rb
+                ){
+            getAlert(getString(R.string.validacion_aviso), getString(R.string.seleccione_pregunta) + getString(R.string.terrenoAgropecuarioSi));
+            cancel = true;
+            return cancel;
+        }
+
+
+
+
+
 
         return false;
+    }
+
+    /**
+     * Muestra las alertas
+     *
+     * @param title
+     * @param message
+     */
+    private void getAlert(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(message).setTitle(title);
+        builder.setPositiveButton("Aceptar",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    /**
+     * Método para realizar las acciones
+     */
+    private void realizarAcciones() {
+
+        guardarPersonaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validarCampos()){
+                    return;
+                }
+                else
+                {
+                    getAlert("Cris","Si guarda");
+                }
+
+            }
+        });
+
     }
 }
