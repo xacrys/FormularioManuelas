3a2a1c331075bb9ad564ccde01eb818640d83a00
Lorena7 <lorenamorales4455@gmail.com>
2017-08-28 08:57:26 -0500

    Metodo para eliminar personas

--- app/src/main/java/ec/gob/stptv/formularioManuelas/modelo/dao/PersonaDao.java
+++ app/src/main/java/ec/gob/stptv/formularioManuelas/modelo/dao/PersonaDao.java
@@ -29,6 +29,20 @@ public class PersonaDao extends Persona{
         int result = cr.update(FormularioManuelasProvider.CONTENT_URI_PERSONA, values, whereById, new String[] { String.valueOf(persona.getId()) });
         return result;
     }
+    public static int delete(ContentResolver cr, Persona persona) {
+        int filaEliminada;
+
+        filaEliminada = cr.delete(FormularioManuelasProvider.CONTENT_URI_PERSONA,
+                whereById,
+                new String[] { String.valueOf(persona.getId()) });
+
+        if (filaEliminada > 0) {
+            ordenar(cr, persona.getIdvivienda());
+        }
+
+        return filaEliminada;
+    }
+
 
     public static int updateCabezera(ContentResolver cr, Persona persona,
                                      String[] selectionArgs) {
@@ -52,7 +66,7 @@ public class PersonaDao extends Persona{
                 columnas, where, parametros,
                 orderBy);
 
-        ArrayList<Persona> personas = new ArrayList<Persona>();
+        ArrayList<Persona> personas = new ArrayList<>();
 
         if (result.moveToFirst())
             do {
@@ -61,5 +75,48 @@ public class PersonaDao extends Persona{
 
         result.close();
         return personas;
+    }
+
+    public static Persona getPersona(ContentResolver cr, String where, String[] id) {
+
+        Cursor result = cr.query(
+                FormularioManuelasProvider.CONTENT_URI_PERSONA,
+                columnas, where, id, null);
+
+        Persona persona = null;
+
+        if ((result.getCount() == 0) || !result.moveToFirst()) {
+            persona = null;
+
+        } else {
+            if (result.moveToFirst()) {
+
+                persona = newPersona(result);
+
+            }
+        }
+
+        result.close();
+        return persona;
+    }
+
+    /**
+     * Metodo que ordena a als personas cuando elimina
+     * @param cr
+     * @param viviendaId
+     */
+    public static void ordenar(ContentResolver cr, int viviendaId) {
+
+        ArrayList<Persona> personas = getPersonas(cr, Persona.whereByViviendaId, new String[] { String.valueOf(viviendaId) }, Persona.COLUMNA_ORDEN);
+
+        int orden = 1;
+        for (Persona _persona : personas) {
+
+            _persona.setOrden(orden);
+            update(cr, _persona);
+            orden++;
+
+        }
+
     }
 }
